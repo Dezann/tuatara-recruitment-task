b79cc75ec4bd52496b93114bcdde92d4a367f22e
Bartosz Kępka <bartosz.kepka@mayeryn.com>
2020-07-13 13:53:19 +0200

    Add FullHeightSection component and install react-youtube library

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.scss";
 import MainPage from "./Components/MainPage";
+import FullHeightSection from "./Components/FullHeightSection/FullHeightSection";
 
 function App() {
   return (
     <div className="App">
       <MainPage />
+      <FullHeightSection></FullHeightSection>
     </div>
   );
 }
---
+++ src/Components/FullHeightSection/FullHeightSection.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./FullHeightSection.scss";
+
+const FullHeightSection = () => {
+  return <section className="full-height-section"></section>;
+};
+
+export default FullHeightSection;
