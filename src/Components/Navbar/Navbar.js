c346dd2f901ca8ae7fede71d0e3b69c7399ccb06
Bartosz Kępka <bartosz.kepka@mayeryn.com>
2020-07-14 14:39:12 +0200

    Add hamburger menu on mobile

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,27 +1,60 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Navbar.scss";
 
-const Navbar = () => {
-  return (
-    <div className="navbar">
-      <div className="navbar-left-content">
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      navbarOpen: false,
+    };
+
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+  }
+  toggleNavbar() {
+    this.setState({ navbarOpen: !this.state.navbarOpen });
+  }
+
+  render() {
+    return (
+      <div className="navbar">
         <img
           className="logo"
           src="https://upload.wikimedia.org/wikipedia/commons/3/37/Apple_TV%2B_logo.png"
         ></img>
-        <div className="navigation-buttons">
-          <button className="">Phasellus</button>
-          <button className="">Finibus</button>
-          <button className="">Congue & rhoncus</button>
-          <button className="">Vivamus</button>
+        <div
+          className={
+            this.state.navbarOpen ? "nav-links nav-active" : "nav-links"
+          }
+        >
+          <div className="site-links">
+            <ul className="navigation-buttons">
+              <li>
+                <a href="#">Phasellus</a>
+              </li>
+              <li>
+                <a href="#">Finibus</a>
+              </li>
+              <li>
+                <a href="#">Congue & rhoncus</a>
+              </li>
+              <li>
+                <a href="#">Vivamus</a>
+              </li>
+            </ul>
+          </div>
+          <div className="login-buttons">
+            <button className="">Log in</button>
+            <button className="">Register</button>
+          </div>
+        </div>
+        <div className="burger" onClick={this.toggleNavbar}>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
         </div>
       </div>
-      <div className="login-buttons">
-        <button className="">Log in</button>
-        <button className="">Register</button>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Navbar;
